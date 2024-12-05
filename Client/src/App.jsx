fd542abe022bdf90696113aab88ef69c7ccc2e34
ajay1264 <kambleajay146@gmail.com>
2024-12-05 17:17:48 +0530

    Folders created

---
+++ Client/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+  <div className='bg-yellow-500'> Hello</div>
+  <h1 className='bg-black'>Wassp</h1>
+    </>
+  )
+}
+
+export default App
