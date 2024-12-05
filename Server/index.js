54a43ef06c518112084213104485512f16f78af4
ajay1264 <kambleajay146@gmail.com>
2024-12-05 17:52:33 +0530

    MongoDB connected

--- Server/index.js
+++ Server/index.js
@@ -1,1 +1,30 @@
-console.log("hello world");
+//packages 
+import path from 'path'
+import express from 'express'
+import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser'
+
+
+//utilites
+import connectDB from "./config/db.js"
+import exp from 'constants'
+
+
+dotenv.config();
+const port = process.env.PORT || 8000;
+
+connectDB()
+
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(cookieParser())
+
+app.get('/', (req, res) => {
+
+})
+
+app.listen(port, () => console.log(`Server running on port: ${port}`));
+ 
