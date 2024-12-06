35942266bd1b22da57d43cf2adab245a1ab6f012
ajay1264 <kambleajay146@gmail.com>
2024-12-06 16:34:07 +0530

    JWT token created successfully

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -1,6 +1,10 @@
 import User from '../models/userModel.js'
 import asyncHandler from '../middlewares/asyncHandler.js'
 import bcrypt from "bcryptjs"
+import createToken from "../utils/createToken.js"
+import generateToken from '../utils/createToken.js';
+
+
 const createUser = asyncHandler(async (req, res) => {
    const {username, email, password } = req.body
 
@@ -21,6 +25,7 @@ const hashedPassword = await bcrypt.hash(password, salt)
 
    try {
     await newUser.save()
+    createToken(res, newUser._id);
 
     res.status(201).json({
         _id: newUser._id,
